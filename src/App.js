f0bab73198a983a1ec2ed4c18cb2469a381edf2d
Elise Burridge <emjburridge@gmail.com>
2021-10-02 19:03:36 +0100

    added jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from './Pet';
 
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Sidney",
+//       animal: "Snake",
+//       breed: "Corn Snake",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Muka Muka",
+//       animal: "Lizard",
+//       breed: "Argentine Tegu",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Winston",
+//       animal: "Dog",
+//       breed: "Shiba Inu",
+//     }),
+//   ]);
+// };
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Sidney",
-      animal: "Snake",
-      breed: "Corn Snake",
-    }),
-    React.createElement(Pet, {
-      name: "Muka Muka",
-      animal: "Lizard",
-      breed: "Argentine Tegu",
-    }),
-    React.createElement(Pet, {
-      name: "Winston",
-      animal: "Dog",
-      breed: "Shiba Inu",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Sidney" animal="Snake" breed="Corn Snake" />
+      <Pet name="Muka Muka" animal="Lizard" breed="Argentine Tegu" />
+      <Pet name="Winston" animal="Dog" breed="Shiba Inu" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
