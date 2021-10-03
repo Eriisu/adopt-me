55b6068a574cadbb7b593987ee99943d16d6149e
Elise Burridge <emjburridge@gmail.com>
2021-10-03 15:00:36 +0100

    created first hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 // const App = () => {
 //   return React.createElement("div", {}, [
@@ -26,9 +26,7 @@ const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Sidney" animal="Snake" breed="Corn Snake" />
-      <Pet name="Muka Muka" animal="Lizard" breed="Argentine Tegu" />
-      <Pet name="Winston" animal="Dog" breed="Shiba Inu" />
+      <SearchParams />
     </div>
   );
 };
