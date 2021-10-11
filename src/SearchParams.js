ca08cbd5f1a04e934d72ce5a5b698905be900867
Elise Burridge <emjburridge@gmail.com>
2021-10-11 22:26:32 +0100

    fixed search params error

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import useBreedList from './useBreedList';
-import Pet from './Pet';
+import Results from './Results';
 
 const ANIMALS = ['cat', 'dog', 'snake', 'lizard', 'frog', 'bird', 'rabbit', 'reptile'];
 
