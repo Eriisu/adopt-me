9848a22c3cd0b69b67aa2e17e28f88abac4353e0
Elise Burridge <emjburridge@gmail.com>
2021-11-09 13:16:18 +0000

    added strictmode

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import ReactDOM from 'react-dom';
 import SearchParams from './SearchParams';
 
@@ -31,4 +32,9 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
